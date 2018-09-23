d11e3e6c11b43b19ef8a1071aeed9197dc4c0a41
[Andrew Held] <held0010@algonquinlive.com>
2018-09-23 13:41:27 -0400

    Shared Preferences RW

--- app/src/main/java/com/example/andrew/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/andrew/androidlabs/LoginActivity.java
@@ -1,10 +1,19 @@
 package com.example.andrew.androidlabs;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class LoginActivity extends Activity {
+
+    Button login_button;
+    SharedPreferences prefs;
+    TextView login_email;
 
     protected static final String ACTIVITY_NAME = "LoginActivity";
 
@@ -12,7 +21,26 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_login);
+
+        //Error Msg
         Log.i(ACTIVITY_NAME, "in onCreate()");
+
+
+        prefs = getApplicationContext().getSharedPreferences("user_preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = prefs.edit();
+
+        //Read default email
+        prefs.getString("default_email", "email@domain.com");
+        //login_email.setText(prefs.getString("default_email", "email@domain.com"));
+
+        // write default email
+        //edit.putString("default_email", );
+
+        //login_button = ;
+
+
+        // commit
+        edit.commit();
     }
 
    @Override
