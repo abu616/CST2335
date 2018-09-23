6b7dc8374be744385a55f4ac18d1ac0bbd142e25
[Andrew Held] <held0010@algonquinlive.com>
2018-09-23 12:26:18 -0400

    Login_Activity issue fixed.

--- app/src/main/java/com/example/andrew/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/andrew/androidlabs/LoginActivity.java
@@ -1,22 +1,18 @@
 package com.example.andrew.androidlabs;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
-
-import java.util.Set;
 
 public class LoginActivity extends Activity {
 
     protected static final String ACTIVITY_NAME = "LoginActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_login);
+        Log.i(ACTIVITY_NAME, "in onCreate()");
     }
 
    @Override
