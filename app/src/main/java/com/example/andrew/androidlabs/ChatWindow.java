667a79e7f861e1dc521eba527f2b15b365c5a2f2
[Andrew Held] <held0010@algonquinlive.com>
2018-10-01 16:42:23 -0400

    Lab 4
    Chat bugg: check text in TextArray<>

--- app/src/main/java/com/example/andrew/androidlabs/ChatWindow.java
+++ app/src/main/java/com/example/andrew/androidlabs/ChatWindow.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 
 public class ChatWindow extends Activity {
 
-    protected static final String ACTIVITY_NAME="ChatWindow";
+    //protected static final String ACTIVITY_NAME="ChatWindow";
 
     ListView myList;
     EditText inputText;
